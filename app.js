dfa820661e2b7e6543109c1e9da89434f8424997
Erik Puentes <erikpuentes.24@gmail.com>
2025-02-19 12:26:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let amigos=[];
+
+function agregarAmigo(){
+  function verificadorDeTexto(){
+    let nombreAmigo=document.getElementById('amigo').value.trim();   //limpiar caja
+    const soloLetras = /^[a-zA-Z\s]*$/;     //verifica que solo sea letras 
+    console.log(nombreAmigo);
+    
+    if(nombreAmigo === ''){
+      alert("por favor inserta un nombre");
+      return;
+
+    } else if (isNaN (nombreAmigo) && soloLetras.test(nombreAmigo)){    // verifica que sean los caracteres validos
+      amigos.push(nombreAmigo);
+      document.getElementById('amigo').value = "";
+      console.log(amigos);
+      mostrarAmigos();
+
+    } else {
+      alert("Por favor ingresa un nombre valido");
+      document.getElementById('amigo').value = "";
+      return;
+    
+    }
+  }
+
+  verificadorDeTexto ();
+
+  function mostrarAmigos(){     // se obtiene el elemento de lista de amigos
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = '';      // se limpia la lista de amigos
+        
+ // itera sobre el arreglo y agrega el elemento a la lista
+
+    for(let i = 0 ; i < amigos.length; i++ ){
+      const nuevoAmigo = document.createElement("li");
+      nuevoAmigo.textContent = amigos[i];
+      listaAmigos.appendChild(nuevoAmigo); 
+    }
+  }
+}  
+
+function sortearAmigo() {// validar que alla amigos en la lista
+    if (amigos.length === 0) {
+    document.getElementById("resultado").innerHTML = "No hay amigos para sortear"; // error si no hay nombres para sortear
+     return;
+  }
+
+  // Genera un índice aleatorio
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);// generar el indice aleatorio
+      
+  // Obtiene el nombre del amigo sorteado
+  const amigoSorteado = amigos[indiceAleatorio];// obtener un nombre aleatorio
+      
+  // Muestra el resultado
+  document.getElementById("resultado").innerHTML= `El amigo sorteado es: ${amigoSorteado}`; //resultado del sorteo
+}
+
+
+     
